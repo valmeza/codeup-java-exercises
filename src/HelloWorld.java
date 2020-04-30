c1709760c2903f76b669bbf7489ea44abf88949e
Valeria Meza <valeriameza173@gmail.com>
2020-04-30 16:50:22 -0500

    completed the first part of the Java lecture.

---
+++ src/HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello world from a new line!");
+        System.out.print("Hello World w/o new line");
+        System.out.print("Hello World w/o new line");
+    }
+}
