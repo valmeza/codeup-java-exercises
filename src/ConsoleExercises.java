6dad78d5762a4c863607d42d3d522964c07d90eb
Valeria Meza <valeriameza173@gmail.com>
2020-05-01 18:25:09 -0500

    minor changes

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -3,26 +3,26 @@ import java.util.Scanner;
 public class ConsoleExercises {
     public static void main (String[] args) {
         Scanner scanner = new Scanner (System.in);
-//        double pi = 3.14159;
-//        System.out.format("The value of pi is approximately %.2f%n", pi);
-//
-//
-//        System.out.println("Enter a number: ");
-//        int number = scanner.nextInt();
-//
-//        System.out.println("Number Entered: " + number);
-//
-//        System.out.println("Enter three words: ");
-//        String word1 = scanner.next();
-//        String word2 = scanner.next();
-//        String word3 = scanner.next();
-//
-//        System.out.printf("Words Entered: %s, %s, %s \n", word1, word2, word3);
-//        scanner.nextLine();
-//
-//        System.out.println("Write a sentence: ");
-//        String sentenceInput = scanner.nextLine();
-//        System.out.println(sentenceInput + "\n");
+        double pi = 3.14159;
+        System.out.format("The value of pi is approximately %.2f%n", pi);
+
+
+        System.out.println("Enter a number: ");
+        int number = scanner.nextInt();
+
+        System.out.println("Number Entered: " + number);
+
+        System.out.println("Enter three words: ");
+        String word1 = scanner.next();
+        String word2 = scanner.next();
+        String word3 = scanner.next();
+
+        System.out.printf("Words Entered: %s, %s, %s \n", word1, word2, word3);
+        scanner.nextLine();
+
+        System.out.println("Write a sentence: ");
+        String sentenceInput = scanner.nextLine();
+        System.out.println(sentenceInput + "\n");
 
 //        System.out.println("Enter length and width:");
 //        int length = scanner.nextInt();
