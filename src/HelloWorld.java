bbe2c64798cb693f8d12cdc5fb10af245372657d
Valeria Meza <valeriameza173@gmail.com>
2020-05-01 09:26:14 -0500

    added int favnum

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,6 +2,12 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello world from a new line!");
         System.out.print("Hello World w/o new line");
-        System.out.print("Hello World w/o new line");
+        System.out.print("Hello World w/o new line \n");
+
+        // Exercise
+
+        int myFavoriteNumber = 100;
+        System.out.println(myFavoriteNumber);
+
     }
 }
