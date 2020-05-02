b66e983deac2a57f29a5f37500a4f0fed80a100c
Valeria Meza <valeriameza173@gmail.com>
2020-05-01 21:59:34 -0500

    added another way to do the last part of the exercise.

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -32,13 +32,25 @@ public class ConsoleExercises {
 //        System.out.printf("The area is: %d \n", area);
 //        System.out.printf("The perimeter is: %d", perimeter);
 
-        System.out.print("Width: \n");
-        String width = scanner.nextLine();
-        System.out.print("Length: \n");
-        String length = scanner.nextLine();
-        int perimeter = (Integer.parseInt(width) * 2) + (Integer.parseInt(length) * 2);
-        int area = (Integer.parseInt(width) * Integer.parseInt(length));
-        System.out.printf("Perimeter: %d%n", perimeter);
-        System.out.printf("Area: %d%n", area);
+//        System.out.print("Width: \n");
+//        String width = scanner.nextLine();
+//        System.out.print("Length: \n");
+//        String length = scanner.nextLine();
+//        int perimeter = (Integer.parseInt(width) * 2) + (Integer.parseInt(length) * 2);
+//        int area = (Integer.parseInt(width) * Integer.parseInt(length));
+//        System.out.printf("Perimeter: %d%n", perimeter);
+//        System.out.printf("Area: %d%n", area);
+
+        System.out.println("Give me a Width");
+        float width = Float.parseFloat(scanner.nextLine());
+
+        System.out.println("Give me a Length");
+        float length = Float.parseFloat(scanner.nextLine());
+
+        float area = width * length;
+        float perimeter = (width * 2) + (length * 2);
+
+        System.out.println("Area: " + area);
+        System.out.println("Perimeter: " + perimeter);
     }
 }
