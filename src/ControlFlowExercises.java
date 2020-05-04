c77447dd7d47cb1500a992f57070c668644a5418
Valeria Meza <valeriameza173@gmail.com>
2020-05-04 15:03:57 -0500

    Completed exercises.

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,11 +1,13 @@
+import org.w3c.dom.ls.LSOutput;
+
 import java.util.Scanner;
 
 public class ControlFlowExercises {
-    public static void main (String[] args) {
+    public static void main(String[] args) {
         // 1. Loop Basics
         // a. While
         int i = 5;
-        while(i <= 15) {
+        while (i <= 15) {
             System.out.println("While: i = " + i);
             i++;
         }
@@ -13,41 +15,41 @@ public class ControlFlowExercises {
         int a = 0;
         do {
             System.out.println("Do while: a = " + a);
-            a+=2;
+            a += 2;
         } while (a <= 100);
 
         int b = 100;
         do {
             System.out.println("Count backwards: b = " + b);
-            b-=5;
+            b -= 5;
         } while (b >= -10);
 
         long c = 2L;
         do {
             System.out.println("c = " + c);
-            c*=c;
+            c *= c;
         } while (c < 1000000);
 
         // c. for
-        for(int y = 5; y <= 15; y++) {
+        for (int y = 5; y <= 15; y++) {
             System.out.println("for loop: y = " + y);
         }
 
-        for(int l = 0; l <= 100; l+=2) {
+        for (int l = 0; l <= 100; l += 2) {
             System.out.println("for loop: l = " + l);
         }
 
-        for(int backward = 100; backward >= -10; backward-=5) {
+        for (int backward = 100; backward >= -10; backward -= 5) {
             System.out.println("for loop: backward = " + backward);
         }
 
-        for(long n = 2L; n < 1000000; n*=n) {
+        for (long n = 2L; n < 1000000; n *= n) {
             System.out.println("for loop: n = " + n);
         }
 
         // 2. FizzBuzz
-        for(int fizzbuzz = 1; fizzbuzz <= 100; fizzbuzz++) {
-            if(fizzbuzz % 3 == 0 && fizzbuzz % 5 == 0) {
+        for (int fizzbuzz = 1; fizzbuzz <= 100; fizzbuzz++) {
+            if (fizzbuzz % 3 == 0 && fizzbuzz % 5 == 0) {
                 System.out.println("FizzBuzz");
             } else if (fizzbuzz % 3 == 0) {
                 System.out.println("Fizz");
@@ -64,8 +66,8 @@ public class ControlFlowExercises {
         int inputSquared = Integer.parseInt(scanner.nextLine());
 
         System.out.println("Number" + "   |" + "Squared" + "   |" + "Cubed");
-        for(int squared = 1; squared <= inputSquared; squared++){
-            System.out.println("Number: " + squared +"| "+ "Squared: " + squared * squared + "| " + "Cubed: " + squared * squared * squared);
+        for (int squared = 1; squared <= inputSquared; squared++) {
+            System.out.println("Number: " + squared + "| " + "Squared: " + squared * squared + "| " + "Cubed: " + squared * squared * squared);
             System.out.println("____________________________________");
         }
 
@@ -77,19 +79,23 @@ public class ControlFlowExercises {
 //        D : 66 - 60
 //        F : 59 - 0
 
-        System.out.println("Enter a number from 0 - 100");
-        int result = Integer.parseInt(scanner.nextLine());
-
-        if(result >= 88) {
-            System.out.println("Grade: A");
-        } else if(result >= 87) {
-            System.out.println("Grade: B");
-        } else if(result >= 79) {
-            System.out.println("Grade: C");
-        } else if(result >= 66) {
-            System.out.println("Grade: D");
-        } else {
-            System.out.println("Grade: F");
-        }
+        String confirmation = "";
+        do {
+            System.out.println("Enter a number from 0 - 100");
+            int result = Integer.parseInt(scanner.nextLine());
+            if (result >= 88) {
+                System.out.println("Grade: A");
+            } else if (result >= 87) {
+                System.out.println("Grade: B");
+            } else if (result >= 79) {
+                System.out.println("Grade: C");
+            } else if (result >= 66) {
+                System.out.println("Grade: D");
+            } else {
+                System.out.println("Grade: F");
+            }
+            System.out.println("Do you wish to continue? [y/n]");
+            confirmation = scanner.nextLine();
+        } while(confirmation.equalsIgnoreCase("Y"));
     }
 }
