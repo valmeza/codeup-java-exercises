335d493e845723b8024901ed6a5c051163f8415a
Valeria Meza <valeriameza173@gmail.com>
2020-05-05 10:02:11 -0500

    completed the basics of Strings.

---
+++ src/StringExercise.java
@@ -0,0 +1,15 @@
+public class StringExercise {
+    public static void main(String[] args) {
+        String sentence = "We don't need no education\n" + "We don't need no thought control";
+        System.out.println(sentence);
+
+        String anotherSentence = "Check \"this\" out!, \"s inside of \"s!";
+        System.out.println(anotherSentence);
+
+        String word = "In windows, the main drive is usually C:\\";
+        System.out.println(word);
+
+        String backSlashes = "I can do backslashes \\, double backslashes \\\\,\n" + "and the amazing triple backslash \\\\\\!";
+        System.out.println(backSlashes);
+    }
+}
