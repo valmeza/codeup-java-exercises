5de8db29ac485ad5d11c7c799b3f4f85d3a6a0e5
Valeria Meza <valeriameza173@gmail.com>
2020-05-07 16:19:15 -0500

    completed part of the first exercise.

---
+++ src/Person.java
@@ -0,0 +1,23 @@
+public class Person {
+    private String name;
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void sayHello() {
+        System.out.println("Hello! " + this.name + "!");
+    }
+
+    public static void main(String[] args){
+        Person person1 = new Person();
+        person1.setName("Val");
+        System.out.println(person1.getName());
+        person1.sayHello();
+    }
+
+}
