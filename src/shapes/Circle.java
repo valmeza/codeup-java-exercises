df4a652d678ed376be968fa63fb920fe21c43836
Valeria Meza <valeriameza173@gmail.com>
2020-05-08 12:25:31 -0500

    completed the bonus

--- src/shapes/Circle.java
+++ src/shapes/Circle.java
@@ -2,11 +2,17 @@ package shapes;
 
 public class Circle {
     private double radius;
+    static private int count = 0; // Bonus: Created property set it to static private so no one changes the value
     private double pi = Math.PI;
 
     // There's no circle without a radius
     public Circle (double radius) {
         this.radius = radius;
+        count++; // Bonus count every new circle
+    }
+
+    public static int getCount() {
+        return count;
     }
 
     public double getArea() {
--- src/shapes/CircleApp.java
+++ src/shapes/CircleApp.java
@@ -4,8 +4,14 @@ import util.Input;
 public class CircleApp {
     public static void main (String[] args) {
         Input input = new Input(); // this will import our Input Class
-        Circle circle = new Circle(input.getDouble());
-        System.out.println("Circle Area: " + circle.getArea());
-        System.out.println("Circle Circumference: " + circle.getCircumference());
+//        Circle circle = new Circle(input.getDouble()); // calls the Circle Class
+        Circle circle;
+        // Bonus: Use yesNo
+        while(input.yesNo()) {
+            circle = new Circle(input.getDouble());
+            System.out.println("Area: " + circle.getArea());
+            System.out.println("Circumference: " + circle.getCircumference());
+        }
+        System.out.println("Count: " + Circle.getCount());
     }
 }
