101290a1beda6b8bca1999a92e5e49939cf23691
Valeria Meza <valeriameza173@gmail.com>
2020-05-08 12:13:06 -0500

    added helpful notes

--- src/shapes/CircleApp.java
+++ src/shapes/CircleApp.java
@@ -3,7 +3,7 @@ import util.Input;
 
 public class CircleApp {
     public static void main (String[] args) {
-        Input input = new Input();
+        Input input = new Input(); // this will import our Input Class
         Circle circle = new Circle(input.getDouble());
         System.out.println("Circle Area: " + circle.getArea());
         System.out.println("Circle Circumference: " + circle.getCircumference());
