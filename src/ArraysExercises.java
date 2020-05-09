71c3d43856dd98a95fe8e598df82530e9bc2039d
Valeria Meza <valeriameza173@gmail.com>
2020-05-08 20:22:30 -0500

    finally figured out the second part of the Person class.

--- src/ArraysExercises.java
+++ src/ArraysExercises.java
@@ -4,6 +4,24 @@ public class ArraysExercises {
     public static void main(String[] args) {
         int[] numbers = {1, 2, 3, 4, 5};
 //        System.out.println(numbers); // this will the return the address of numbers
-        System.out.println(Arrays.toString(numbers)); // using Arrays.toString() returns the actual elements of the number array
+//        System.out.println(Arrays.toString(numbers)); // using Arrays.toString() returns the actual elements of the number array
+          String[] person  = new String[3];
+          Person person1 = new Person("Valera");
+          Person person2 = new Person("Leo");
+          Person person3 = new Person("Gina");
+
+          person[0] = person1.getName();
+          person[1] = person2.getName();
+          person[2] = person3.getName();
+
+        //sout: to better understand what is happening.
+        System.out.println(Arrays.toString(person)); // returns the array address
+        System.out.println(Arrays.toString(person)); // returns the elements in the array
+
+        for(int i = 0; i < person.length; i++) {
+            System.out.println("Looping: " + person[i]);
+        }
+
     }
+
 }
--- src/Person.java
+++ src/Person.java
@@ -20,6 +20,10 @@ public class Person {
 
     public static void main(String[] args) {
         Person person1 = new Person("Valeria");
-        person1.sayHello();
+        Person person2 = new Person("Leo");
+        Person person3 = new Person("Gina");
+        System.out.println(person1.getName());
+        System.out.println(person2.getName());
+        System.out.println(person3.getName());
     }
 }
