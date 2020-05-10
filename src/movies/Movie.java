f100b504861b03099587b2758bfa807685e74555
Valeria Meza <valeriameza173@gmail.com>
2020-05-10 12:51:01 -0500

    working on the movies application exercise.

--- src/movies/Movie.java
+++ src/movies/Movie.java
@@ -3,4 +3,24 @@ package movies;
 public class Movie {
     private String name;
     private String category;
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name, String cat) {
+        this.name = name;
+    }
+
+    public String getCategory() {
+        return this.category;
+    }
+
+    public void setCategory() {
+        this.setCategory();
+    }
+
+    public Movie(String citizen_kane, String drama) {
+        
+    }
 }
---
+++ src/movies/MoviesApplication.java
@@ -0,0 +1,9 @@
+package movies;
+import util.Input;
+
+public class MoviesApplication {
+    public static void main(String[] args) {
+        MoviesArray selectMovie = new MoviesArray();
+        selectMovie.findAll();
+    }
+}
