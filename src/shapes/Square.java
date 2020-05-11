153620ae4bbd12878b75572f0c36131aa059d3dc
Valeria Meza <valeriameza173@gmail.com>
2020-05-11 16:05:02 -0500

    Made another way to override another method.

--- src/shapes/Square.java
+++ src/shapes/Square.java
@@ -6,4 +6,14 @@ public class Square extends Rectangle{
         super(side, side); // grabs the parents constructor.
     }
 
+
+    // override method
+
+    public int getArea() {
+        return (int) Math.pow(super.width, 2); // width to the second power
+    }
+
+    public int getPerimeter() {
+        return 4 * super.length;
+    }
 }
