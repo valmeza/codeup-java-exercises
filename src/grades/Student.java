e5122839ff43f198b064e4a25483b6b43794545d
Valeria Meza <valeriameza173@gmail.com>
2020-05-13 14:41:04 -0500

    added the first part of the collections exercise.

---
+++ src/grades/Student.java
@@ -0,0 +1,29 @@
+package grades;
+
+import java.util.ArrayList;
+
+public class Student {
+    private String name;
+    private ArrayList<Integer> grades;
+
+    public Student(String name) {
+        this.name = name;
+        this.grades = new ArrayList<>();
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void addGrade(int grade) {
+        this.grades.add(grade);
+    }
+
+    public double getGradeAverage() {
+        double sum = 0;
+        for(int i = 0; i<this.grades.size(); i++) {
+            sum += i;
+        }
+        return sum / this.grades.size();
+    }
+}
