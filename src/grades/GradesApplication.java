cf8e766b86447dc7e8fde943a6eb74f36cbe8d64
Valeria Meza <valeriameza173@gmail.com>
2020-05-14 09:32:55 -0500

    fixed the name and user name search.

--- src/grades/GradesApplication.java
+++ src/grades/GradesApplication.java
@@ -9,30 +9,31 @@ public class GradesApplication {
         // Value is the Student object
         HashMap<String, Student> students = new HashMap<>();
 
-        Student john = new Student("tyrannosaurusRex");
+        Student john = new Student("john");
         john.addGrade(100);
         john.addGrade(83);
         john.addGrade(90);
 
-        Student luke = new Student ("lukeSkywalker");
+        Student luke = new Student ("luke");
         luke.addGrade(40);
         luke.addGrade(50);
         luke.addGrade(4);
 
-        Student maria = new Student("cinderella");
+        Student maria = new Student("maria");
         maria.addGrade(90);
         maria.addGrade(87);
         maria.addGrade(79);
 
-        Student kyle = new Student("mrCool247");
+        Student kyle = new Student("kyle");
         kyle.addGrade(9);
         kyle.addGrade(43);
         kyle.addGrade(3);
 
-        students.put(john.getName(), john);
-        students.put(luke.getName(), luke);
-        students.put(maria.getName(), maria);
-        students.put(kyle.getName(), kyle);
+        students.put("tyrannnosaurusRex", john);
+        students.put("lukeSkyWalker", luke);
+        students.put("cinderella", maria);
+        students.put("mrCool247", kyle);
+
         welcome(students);
     }
 
@@ -64,14 +65,14 @@ public class GradesApplication {
     }
 
     public static void response(HashMap<String, Student> students, String username) {
-
-
         Student match;
+        Student grades;
 
         if(!students.containsKey(username)) {
             System.out.println("Sorry, no student was found with the Github username of " + username);
         } else {
             match = students.get(username);
+//            grades = students.get(username);
             System.out.println("Name: " + match.getName() + " Github: " + username);
             System.out.printf("Current Grade Average: %,.1f%n", match.getGradeAverage());
         }
