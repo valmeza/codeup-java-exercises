3231ee7637fddb7b6aeae932f7c40b4fffc8d804
Valeria Meza <valeriameza173@gmail.com>
2020-05-15 10:01:41 -0500

    completed the Exeptions and Error Handling exercise.

--- src/util/Input.java
+++ src/util/Input.java
@@ -7,7 +7,7 @@ public class Input {
     private Scanner scanner = new Scanner(System.in);
 
     public String getString() {
-        System.out.println("Enter Something: ");
+//        System.out.println("Enter Something: ");
         return scanner.nextLine();
     }
 
@@ -18,31 +18,52 @@ public class Input {
 
     public int getInt(int min, int max) {
         System.out.println("Enter a number between " + min + " " + max);
-        int number = Integer.parseInt(scanner.nextLine());
-        if (number < min || number > max) {
-            System.out.println("Out of range!");
-            return getInt(min, max);
+        int number = 0;
+        try {
+            number = Integer.valueOf(getString());
+            if (number < min || number > max) {
+                System.out.println("Out of range!");
+                return getInt(min, max);
+            }
+        } catch (NumberFormatException e) {
+            System.out.println("1. Invalid format.");
         }
         return number;
     }
 
     public int getInt() {
-        System.out.println("Enter a number: ");
-        return Integer.parseInt(scanner.nextLine());
+        int number = 0;
+        try {
+           number = Integer.valueOf(getString());
+        } catch (NumberFormatException e) {
+            System.out.println("2. Invalid format");
+        }
+        return number;
     }
 
     public double getDouble(double min, double max) {
         System.out.println("Enter a decimal between " + min + " " + max);
-        double number = Double.parseDouble(scanner.nextLine());
-        if (number < min || number > max) {
-            System.out.println("Out of range!");
-            return getDouble(min, max);
+        double number = 0;
+        try {
+            number = Double.parseDouble(scanner.nextLine());
+            if (number < min || number > max) {
+                System.out.println("Out of range!");
+                return getDouble(min, max);
+            }
+        } catch(NumberFormatException e) {
+            System.out.println("Invalid format");
         }
         return number;
     }
 
     public double getDouble() {
         System.out.println("Enter a decimal: ");
-        return Double.parseDouble(scanner.nextLine());
+        double number  = 0;
+        try {
+            number = Double.valueOf(getString());
+        } catch (NumberFormatException e) {
+            System.out.println("Invalid format");
+        }
+        return number;
     }
 }
