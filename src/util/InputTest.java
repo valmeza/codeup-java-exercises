35e10a981394abfa46ac659fbee85c6955ed8f3d
Valeria Meza <valeriameza173@gmail.com>
2020-05-15 11:49:33 -0500

    completed the small changes

--- src/util/InputTest.java
+++ src/util/InputTest.java
@@ -3,8 +3,8 @@ package util;
 public class InputTest {
     public static void main(String[] args) {
         Input response = new Input();
-//        System.out.println("String: " + response.getString());
-//        System.out.println("Boolean: " + response.yesNo());
+        System.out.println("String: " + response.getString());
+        System.out.println("Boolean: " + response.yesNo());
         System.out.println("Integer: " + response.getInt(1, 10));
         System.out.println("Int: " + response.getInt());
         System.out.println("Decimal " + response.getDouble(1.0, 10.00));
